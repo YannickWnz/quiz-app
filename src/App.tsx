cf9add6c9764ce0dd6fd5cd66737c2b4a7622ef1
yannickwnz <yannickwnz@gmail.com>
2023-08-02 22:13:02 +0000

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import * as React from "react"
 import './App.css'
+import { Quiz } from "./Components/Quiz/Quiz"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Register } from "./pages/Register/Register"
+import { Login } from "./pages/Login/Login"
+import { PrivateRoute } from "./Components/PrivateRoute/PrivateRoute"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div className="container">
-        
-      </div>
+      <Router>
+        <Routes>
+          {/* <Route path='/' element={<Quiz />} ></Route> */}
+          <Route 
+          path='/' 
+          element={
+            <PrivateRoute>
+              <Quiz />
+            </PrivateRoute>
+          } ></Route>
+          <Route path='/register' element={ <Register /> } ></Route>
+          <Route path='/login' element={ <Login /> } ></Route>
+          {/* < */}
+        </Routes>
+      </Router>
     </>
   )
 }
