425d8492c55b4a2cc1402225a6943020dc6ad668
yannickwnz <yannickwnz@gmail.com>
2023-08-02 22:10:36 +0000

    quiz questions

---
+++ src/QuizQuestion/QuizQuestion.ts
@@ -0,0 +1,22 @@
+export const QuizQuestion = [
+    {
+        question: "What happened in 1889",
+        answers: [ 'total solar eclipse', 'tesla died', 'argentina won a world cup' ],
+        correctAnswer: 'total solar eclipse'
+    },
+    {
+        question: "Who is the current president of Peru",
+        answers: [ 'Alex HORMOZI', 'Paul BIYA', 'Dina BOLUARTE' ],
+        correctAnswer: 'Dina BOLUARTE'
+    },
+    {
+        question: "When was the previous World Cup ? And which team won ?",
+        answers: [ 'Sri Lanka, 2018', 'Argentina, 2022', 'Peru, 2016', 'France, 2021' ],
+        correctAnswer: 'Argentina, 2022'
+    },
+    {
+        question: "Who is the richest person in the world",
+        answers: [ 'Lio Messi', 'Bernard Arnault', 'Elon Musk', 'Paul Biya' ],
+        correctAnswer: 'Elon Musk'
+    }
+]
